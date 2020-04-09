a192bb32e224c6d27aeedf28c95889e2d8f3831b
Gabe Ewusk <gabeewsuk@gmail.com>
2020-04-09 16:31:15 -0600

    assignment5 python code and csv file

---
+++ Assignment 5/SEMINARPRPOJECT.py
@@ -0,0 +1,14 @@
+import random
+def generation(file_name):
+    infile = open(file_name, 'r')
+    first = infile.readline()
+    x = random.randint(1,560)
+    z = 0
+    for line in infile:
+        z += 1
+        cells = line.split(',')
+        if z == x:
+            break
+    name = cells[0]
+    print(name)
+generation('namesfixed.csv')
